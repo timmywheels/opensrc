20ef8815c2280d95b1155db41e908eb69398a8d1
Tim Wheeler <tim@timwheeler.com>
2018-10-24 21:36:26 -0400

    edited path const in index.js

--- index.js
+++ index.js
@@ -30,12 +30,13 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 
 	const path = require('path').default;
-
 	// Express will serve up index.html
 	// If it doesn't recognize route
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	});
+
+	// app.get('*', (req, res) => {
+	// 	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	// });
+
 }
 
 const PORT = process.env.PORT || 5000;
