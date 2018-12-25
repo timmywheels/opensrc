8393bdd7e573dcd96118c395ccf7f3c7bed11b82
Tim Wheeler <tim@timwheeler.com>
2018-12-25 08:14:11 -0500

    edited index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
