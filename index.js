d381ac940bd1a6f20e4d0bed79b6c90d8860aaf7
Tim Wheeler <tim@timwheeler.com>
2018-12-30 20:10:06 -0500

    login/logout working, still needs bug fixes

--- index.js
+++ index.js
@@ -44,7 +44,6 @@ if (process.env.NODE_ENV === 'production') {
 	// Express will serve up production assets
 	// Like main.js or main.css
 	app.use(express.static('client/build'));
-	app.use("*", express.static('client/build'));
 
 	const path = require('path').default;
 	// Express will serve up index.html
