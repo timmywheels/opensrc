cd20d53cc097eaffa7fc01e1b735384bd5298fce
Tim Wheeler <tim@timwheeler.com>
2018-12-30 19:07:17 -0500

    added COOKIE_KEY to config/prod.js

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,8 @@
 module.exports = {
-    mongo: process.env.MONGO_URI
+	mongo: process.env.MONGO_URI,
+	github_client_id: process.env.GITHUB_CLIENT_ID,
+	github_client_secret: process.env.GITHUB_CLIENT_SECRET,
+	cookie: {
+		key: process.env.COOKIE_KEY
+	}
 };
