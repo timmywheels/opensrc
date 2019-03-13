7c8bcdc560c856f2d7151339e631dc47ab104adf
Tim Wheeler <tim@timwheeler.com>
2019-03-12 23:52:39 -0400

    edited index.js to serve index.html for all routes

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ if (process.env.NODE_ENV === 'production') {
 	// Express will serve up index.html
 	// If it doesn't recognize route
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+		res.sendFile(path.resolve('client', 'build', 'index.html'));
 	});
 
 }
