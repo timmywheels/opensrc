0773fd21b7ebc288c367fef2158592a5545ca9d0
Tim Wheeler <tim@timwheeler.com>
2019-03-13 23:56:09 -0400

    edited index.js

--- index.js
+++ index.js
@@ -10,27 +10,27 @@ require('./services/passport');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(
-	keys.mongo,
-	{useNewUrlParser: true},
-	(err, db) => {
-		if (err) {
-			console.log(err);
-		} else {
-			console.log(`Connected to ${db.db.databaseName}`);
-		}
-	}
+    keys.mongo,
+    { useNewUrlParser: true },
+    (err, db) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(`Connected to ${ db.db.databaseName }`);
+        }
+    }
 );
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(
-	cookieSession({
-		maxAge: 30 * 24 * 60 * 60 * 1000,
-		keys: [keys.cookie.key],
-	}),
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookie.key],
+    }),
 );
 
 app.use(passport.initialize());
@@ -41,22 +41,23 @@ require('./routes/trendingRepoRoutes')(app);
 require('./routes/userRepoRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
-	// Express will serve up production assets
-	// Like main.js or main.css
-	app.use(express.static('client/build'));
+    // Express will serve up production assets
+    // Like main.js or main.css
+    app.use(express.static('client/build'));
 
-	const path = require('path').default;
-	// Express will serve up index.html
-	// If it doesn't recognize route
-	app.use('*', (req, res) => {
-		// res.sendFile(path.resolve('client', 'build', 'index.html'));
-		res.sendFile(express.static('client/build'));
+    const path = require('path').default;
+    // Express will serve up index.html
+    // If it doesn't recognize route
+    app.get('*', (req, res) => {
+        // res.sendFile(path.resolve('client', 'build', 'index.html')); // original
+        let url = path.join(__dirname, '/client/build', 'index.html');
+        res.sendFile(url);
+    });
 
-	});
 
 }
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-	console.log(`App listening on port ${PORT}`);
+    console.log(`App listening on port ${ PORT }`);
 });
