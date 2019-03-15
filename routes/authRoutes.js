1b0e640f158678301b9a5c7161371178c54416cb
Tim Wheeler <tim@timwheeler.com>
2019-03-15 02:10:41 -0400

    edited authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,13 +10,15 @@ module.exports = app => {
     app.get('/auth/github',
         passport.authenticate('github', {
             scope: ['user', 'repo']
-        })
-    )
+        }));
+
 
     app.get('/auth/github/callback',
-            passport.authenticate("github"), (req, res) => {
-            res.redirect('/dashboard');
-    });
+        passport.authenticate('github', { failureRedirect: '/' }),
+        (req, res) => {
+            // Successful authentication, redirect home.
+            res.redirect('/');
+        });
 
     app.get('/auth/logout', (req, res) => {
         // res.setHeader("Content-Type", "text/html")
